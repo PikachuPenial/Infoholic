e593a404892152a04dc95c110151181d9d5bf04b
Penial <62630906+PikachuPenial@users.noreply.github.com>
2022-03-18 17:04:49 -0700

    More stuff 3 hehe ha
    eeeeeeeee

--- MonoBehaviours/GameStatusUpdate.cs
+++ MonoBehaviours/GameStatusUpdate.cs
@@ -17,6 +17,18 @@ namespace Infoholic.MonoBehaviours
 
         private static TextMeshProUGUI healthText;
 
+        private static TextMeshProUGUI livesText;
+
+        private static TextMeshProUGUI movementSpeedText;
+
+        private static TextMeshProUGUI damageText;
+
+        private static TextMeshProUGUI attackSpeedText;
+
+        private static TextMeshProUGUI reloadTimeText;
+
+        private static TextMeshProUGUI ammoText;
+
         private static Player player;
 
         private Player getLocalPlayer()
@@ -30,7 +42,15 @@ namespace Infoholic.MonoBehaviours
         {
             player = getLocalPlayer();
             healthText = gameObject.AddComponent<TextMeshProUGUI>();
+            livesText = gameObject.AddComponent<TextMeshProUGUI>();
+            damageText = gameObject.AddComponent<TextMeshProUGUI>();
+            movementSpeedText = gameObject.AddComponent<TextMeshProUGUI>();
+            attackSpeedText = gameObject.AddComponent<TextMeshProUGUI>();
+            reloadTimeText = gameObject.AddComponent<TextMeshProUGUI>();
+            ammoText = gameObject.AddComponent<TextMeshProUGUI>();
             gameObject.transform.position = new Vector3(0f, 0f, 0f);
+
+            gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
         }
 
         public void Update()
@@ -40,7 +60,25 @@ namespace Infoholic.MonoBehaviours
                 ToggleGameUI.disableStats();
                 healthText.text = $"Health: [{player.data.health:f2} / {player.data.maxHealth:f2}]";
                 healthText.alpha = 1f;
-                healthText.fontSize = 50;
+                healthText.fontSize = 2;
+                livesText.text = $"Lives: [{player.data.health:f2} / {player.data.stats.respawns:f2}]";
+                livesText.alpha = 1f;
+                livesText.fontSize = 2;
+                movementSpeedText.text = $"Movement Speed: [{player.data.stats.movementSpeed:f2}]";
+                movementSpeedText.alpha = 1f;
+                movementSpeedText.fontSize = 2;
+                damageText.text = $"Damage: [{player.data.weaponHandler.gun.damage:f2}]";
+                damageText.alpha = 1f;
+                damageText.fontSize = 2;
+                attackSpeedText.text = $"Attack Speed: [{player.data.weaponHandler.gun.attackSpeed:f2}]";
+                attackSpeedText.alpha = 1f;
+                attackSpeedText.fontSize = 2;
+                reloadTimeText.text = $"Reload Time: [{player.data.weaponHandler.gun.reloadTime:f2}]";
+                reloadTimeText.alpha = 1f;
+                reloadTimeText.fontSize = 2;
+                ammoText.text = $"Ammo: [{player.data.weaponHandler.gun.ammo:f2}]";
+                ammoText.alpha = 1f;
+                ammoText.fontSize = 2;
             }
         }
     }
