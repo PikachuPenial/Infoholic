17d9570889da945f6b507488c64824feb634375e
Penial <62630906+PikachuPenial@users.noreply.github.com>
2022-03-18 22:32:52 -0700

    0.0.1
    amogus

--- Infoholic.cs
+++ Infoholic.cs
@@ -8,8 +8,9 @@ using UnboundLib;
 using UnboundLib.GameModes;
 using UnboundLib.Utils.UI;
 using Infoholic.MonoBehaviours;
-using Infoholic.UI;
+using Infoholic.Utilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Infoholic
 {
@@ -25,6 +26,9 @@ namespace Infoholic
         private const string ModName = "Infoholic";
         public const string Version = "0.0.1";
         private const string CompatibilityModName = "Infoholic";
+
+        public bool inGame;
+
         public static Infoholic instance { get; private set; }
 
         void Awake()
@@ -37,17 +41,32 @@ namespace Infoholic
 
         void Start()
         {
-            UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] Mod is loaded (somehow.)");
+            UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] Mod is loaded.");
+
+            Unbound.RegisterCredits("<b><color=#09ff00>I</color>nfo<color=#ff0000>h</color>olic</b>", new string[]
+            {
+                "Penial"
+            }, new string[]
+            {
+                "<b><color=#ff00df>GitHub (Source Code)</b></color>",
+                "Penial's Steam Profile",
+            }, new string[]
+            {
+                "https://github.com/PikachuPenial/Infoholic",
+                "https://steamcommunity.com/id/penialsteamlol",
+            });
+
+            Unbound.RegisterMenu("<b>Infoholic Settings</b>", delegate ()
+            {
+            }, new Action<GameObject>(this.NewGUI), null, true);
 
             GameModeManager.AddHook(GameModeHooks.HookGameStart, this.GameStart);
             GameModeManager.AddHook(GameModeHooks.HookGameEnd, this.GameEnd);
-
-            ToggleGameUI.Initialize();
         }
 
         private IEnumerator GameStart(IGameModeHandler gameModeHandler)
         {
-            ToggleGameUI.inGame = true;
+            inGame = true;
             GameStatusUpdate gameStatusUpdate = new GameObject().AddComponent<GameStatusUpdate>();
             UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] inGame is now true.");
             yield break;
@@ -55,9 +74,41 @@ namespace Infoholic
 
         private IEnumerator GameEnd(IGameModeHandler gameModeHandler)
         {
-            ToggleGameUI.inGame = false;
+            inGame = false;
             UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] inGame is now false.");
             yield break;
+        }
+
+        private void NewGUI(GameObject menu)
+        {
+            TextMeshProUGUI textMeshProUGUI;
+            MenuHandler.CreateText("Infoholic Settings", menu, out textMeshProUGUI, 60, true, null, null, null, null);
+            GameObject toggle = MenuHandler.CreateToggle(Infoholic.settingsEnableMod, "<b><color=#09ff00>Enable Mod<b></color>", menu, delegate (bool value)
+            {
+                if (inGame)
+                {
+                    GameStatusUpdate gameStatusUpdate = new GameObject().AddComponent<GameStatusUpdate>();
+                }
+
+                Infoholic.settingsEnableMod = value;
+            }, 50, true, null, null, null, null);
+        }
+
+        public static bool settingsEnableMod
+        {
+            get
+            {
+                return PlayerPrefs.GetInt(Infoholic.GetConfigKey("settingsEnableMod"), 1) == 1;
+            }
+            set
+            {
+                PlayerPrefs.SetInt(Infoholic.GetConfigKey("settingsEnableMod"), value ? 1 : 0);
+            }
+        }
+
+        private static string GetConfigKey(string key)
+        {
+            return "IH_" + key;
         }
     }
 }
--- MonoBehaviours/GameStatusUpdate.cs
+++ MonoBehaviours/GameStatusUpdate.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
@@ -7,8 +11,12 @@ using TMPro;
 using UnboundLib;
 using UnboundLib.GameModes;
 using UnboundLib.Utils.UI;
-using Infoholic.UI;
+using ModdingUtils;
+using ModdingUtils.MonoBehaviours;
+using Infoholic.MonoBehaviours;
+using Infoholic.Utilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Infoholic.MonoBehaviours
 {
@@ -26,6 +34,8 @@ namespace Infoholic.MonoBehaviours
         private static TextMeshProUGUI damageText;
 
         private static TextMeshProUGUI attackSpeedText;
+
+        private static TextMeshProUGUI projectileSpeedText;
 
         private static TextMeshProUGUI reloadTimeText;
 
@@ -46,96 +56,143 @@ namespace Infoholic.MonoBehaviours
 
         private void Start()
         {
+            gameObject.transform.position = new Vector3(-2f, -10f, 0f);
             player = getLocalPlayer();
+
+            // Health
             healthText = new GameObject().AddComponent<TextMeshProUGUI>();
             healthText.gameObject.AddComponent<DestroyOnUnparent>();
             healthText.transform.parent = gameObject.transform;
+            healthText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            healthText.gameObject.transform.position = new Vector3(6f, -18f, 0f);
             healthText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Lives
             livesText = new GameObject().AddComponent<TextMeshProUGUI>();
             livesText.gameObject.AddComponent<DestroyOnUnparent>();
             livesText.transform.parent = gameObject.transform;
+            livesText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            livesText.gameObject.transform.position = new Vector3(6f, -19f, 0f);
             livesText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Block Cooldown
             blockCooldownText = new GameObject().AddComponent<TextMeshProUGUI>();
             blockCooldownText.gameObject.AddComponent<DestroyOnUnparent>();
             blockCooldownText.transform.parent = gameObject.transform;
+            blockCooldownText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            blockCooldownText.gameObject.transform.position = new Vector3(6f, -20f, 0f);
             blockCooldownText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Damage
             damageText = new GameObject().AddComponent<TextMeshProUGUI>();
             damageText.gameObject.AddComponent<DestroyOnUnparent>();
             damageText.transform.parent = gameObject.transform;
+            damageText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            damageText.gameObject.transform.position = new Vector3(6f, -21f, 0f);
             damageText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+
+            // Movement Speed
             movementSpeedText = new GameObject().AddComponent<TextMeshProUGUI>();
             movementSpeedText.gameObject.AddComponent<DestroyOnUnparent>();
             movementSpeedText.transform.parent = gameObject.transform;
+            movementSpeedText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            movementSpeedText.gameObject.transform.position = new Vector3(6f, -22f, 0f);
             movementSpeedText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Attack Speed
             attackSpeedText = new GameObject().AddComponent<TextMeshProUGUI>();
             attackSpeedText.gameObject.AddComponent<DestroyOnUnparent>();
             attackSpeedText.transform.parent = gameObject.transform;
+            attackSpeedText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            attackSpeedText.gameObject.transform.position = new Vector3(6f, -23f, 0f);
             attackSpeedText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Projectile Speed
+            projectileSpeedText = new GameObject().AddComponent<TextMeshProUGUI>();
+            projectileSpeedText.gameObject.AddComponent<DestroyOnUnparent>();
+            projectileSpeedText.transform.parent = gameObject.transform;
+            projectileSpeedText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            projectileSpeedText.gameObject.transform.position = new Vector3(6f, -24f, 0f);
+            projectileSpeedText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Reload Time
             reloadTimeText = new GameObject().AddComponent<TextMeshProUGUI>();
             reloadTimeText.gameObject.AddComponent<DestroyOnUnparent>();
             reloadTimeText.transform.parent = gameObject.transform;
+            reloadTimeText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            reloadTimeText.gameObject.transform.position = new Vector3(6f, -25f, 0f);
             reloadTimeText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Ammo
             ammoText = new GameObject().AddComponent<TextMeshProUGUI>();
             ammoText.gameObject.AddComponent<DestroyOnUnparent>();
             ammoText.transform.parent = gameObject.transform;
+            ammoText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            ammoText.gameObject.transform.position = new Vector3(6f, -26f, 0f);
             ammoText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Bullets
             bulletsText = new GameObject().AddComponent<TextMeshProUGUI>();
             bulletsText.gameObject.AddComponent<DestroyOnUnparent>();
             bulletsText.transform.parent = gameObject.transform;
+            bulletsText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            bulletsText.gameObject.transform.position = new Vector3(6f, -27f, 0f);
             bulletsText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
+
+            // Bursts
             burstsText = new GameObject().AddComponent<TextMeshProUGUI>();
             burstsText.gameObject.AddComponent<DestroyOnUnparent>();
             burstsText.transform.parent = gameObject.transform;
+            burstsText.gameObject.transform.localScale = new Vector3(.4f, .4f, .4f);
+            burstsText.gameObject.transform.position = new Vector3(6f, -28f, 0f);
             burstsText.gameObject.AddComponent<Canvas>().sortingLayerName = "MostFront";
-            gameObject.transform.position = new Vector3(0f, 0f, 0f);
         }
 
         public void Update()
         {
             {
-                UnityEngine.Debug.Log(movementSpeedText);
-                ToggleGameUI.showStats();
-                ToggleGameUI.disableStats();
-                healthText.text = $"Health: [{player.data.health:f2} / {player.data.maxHealth:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] HEALTH STAT WORKS.");
-                healthText.alpha = 1f;
-                healthText.fontSize = 2;
-                livesText.text = $"Lives: [{player.data.health:f2} / {player.data.stats.respawns:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] LIVES STAT WORKS.");
-                livesText.alpha = 1f;
-                livesText.fontSize = 2;
-                blockCooldownText.text = $"Block Cooldown: [{player.data.health:f2} / {player.data.block.cooldown:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] BLOCK COOLDOWN STAT WORKS.");
-                blockCooldownText.alpha = 1f;
-                blockCooldownText.fontSize = 2;
-                movementSpeedText.text = $"Movement Speed: [{player.data.stats.movementSpeed:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] MOVEMENT SPEED STAT WORKS.");
-                movementSpeedText.alpha = 1f;
-                movementSpeedText.fontSize = 2;
-                damageText.text = $"Damage: [{player.data.weaponHandler.gun.damage:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] DAMAGE STAT WORKS.");
-                damageText.alpha = 1f;
-                damageText.fontSize = 2;
-                attackSpeedText.text = $"Attack Speed: [{player.data.weaponHandler.gun.attackSpeed:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] ATTACK SPEED STAT WORKS.");
-                attackSpeedText.alpha = 1f;
-                attackSpeedText.fontSize = 2;
-                reloadTimeText.text = $"Reload Time: [{player.data.weaponHandler.gun.reloadTime:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] RELOAD TIME STAT WORKS.");
-                reloadTimeText.alpha = 1f;
-                reloadTimeText.fontSize = 2;
-                ammoText.text = $"Ammo: [{player.data.weaponHandler.gun.ammo:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] AMMO STAT WORKS.");
-                ammoText.alpha = 1f;
-                ammoText.fontSize = 2;
-                bulletsText.text = $"Bullets: [{player.data.weaponHandler.gun.projectiles:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] BULLET STAT WORKS.");
-                bulletsText.alpha = 1f;
-                bulletsText.fontSize = 2;
-                burstsText.text = $"Bursts: [{player.data.weaponHandler.gun.bursts:f2}]";
-                UnityEngine.Debug.Log($"[{Infoholic.ModInitials}] BURSTS STAT WORKS.");
-                burstsText.alpha = 1f;
-                burstsText.fontSize = 2;
+                if (Infoholic.settingsEnableMod)
+                {
+                    healthText.text = $"Health: [{player.data.health:f0} / {player.data.maxHealth:f0}]";
+                    healthText.alpha = 1f;
+                    healthText.fontSize = 2;
+                    livesText.text = $"Lives: [{player.data.stats.respawns:f0}]";
+                    livesText.alpha = 1f;
+                    livesText.fontSize = 2;
+                    blockCooldownText.text = $"Block Cooldown: [{player.data.block.cooldown:f2}]";
+                    blockCooldownText.alpha = 1f;
+                    blockCooldownText.fontSize = 2;
+                    movementSpeedText.text = $"Movement Speed: [{player.data.stats.movementSpeed:f2}]";
+                    movementSpeedText.alpha = 1f;
+                    movementSpeedText.fontSize = 2;
+                    damageText.text = $"Damage: [{player.data.weaponHandler.gun.damage * 55f:f2}]";
+                    damageText.alpha = 1f;
+                    damageText.fontSize = 2;
+                    attackSpeedText.text = $"Attack Speed: [{(player.data.weaponHandler.gun.attackSpeed * player.data.weaponHandler.gun.attackSpeedMultiplier):f2}]";
+                    attackSpeedText.alpha = 1f;
+                    attackSpeedText.fontSize = 2;
+                    projectileSpeedText.text = $"Projectile Speed: [{(player.data.weaponHandler.gun.projectileSpeed * player.data.weaponHandler.gun.projectielSimulatonSpeed):f2}]";
+                    projectileSpeedText.alpha = 1f;
+                    projectileSpeedText.fontSize = 2;
+                    reloadTimeText.text = $"Reload Time: [{(player.data.weaponHandler.gun.reloadTime + player.data.weaponHandler.gun.reloadTimeAdd):f2}]";
+                    reloadTimeText.alpha = 1f;
+                    reloadTimeText.fontSize = 2;
+                    ammoText.text = $"Ammo: [{player.data.weaponHandler.gun.ammo:f0}]";
+                    ammoText.alpha = 1f;
+                    ammoText.fontSize = 2;
+                    bulletsText.text = $"Bullets: [{player.data.weaponHandler.gun.numberOfProjectiles:f0}]";
+                    bulletsText.alpha = 1f;
+                    bulletsText.fontSize = 2;
+                    burstsText.text = $"Bursts: [{player.data.weaponHandler.gun.bursts:f0}]";
+                    burstsText.alpha = 1f;
+                    burstsText.fontSize = 2;
+                }
+
+                if (!Infoholic.settingsEnableMod)
+                {
+                    Destroy(this.gameObject);
+                }
             }
         }
 
